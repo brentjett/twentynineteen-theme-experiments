0bf65bc2f60582af5a261da88d3c289ea1f28bac
Brent Jett <brentjett@gmail.com>
2019-01-08 15:25:29 -0600

    Removed document wrapper from `header.php` and `footer.php`. Document is being handled by the plugin now.

--- footer.php
+++ footer.php
@@ -50,8 +50,3 @@
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
--- header.php
+++ header.php
@@ -10,16 +10,7 @@
  * @subpackage Twenty_Nineteen
  * @since 1.0.0
  */
-?><!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
+?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
 
